6ab783436ac2edb5ca327c91a78aea5a716da561
MaysStar <123299437+MaysStar@users.noreply.github.com>
2026-06-14 23:03:40 +0300

    feat: develop wi-fi with lwip. Create net event handler.

---
+++ remote_car/app/app_src/app_wifi.c
@@ -0,0 +1,66 @@
+#include "app_wifi.h"
+
+/* Priavte variables and functions */
+static const char* TAG = "APP_WIFI";
+
+static void app_wifi_handler(void* event_handler_arg,
+                            esp_event_base_t event_base,
+                            int32_t event_id,
+                            void* event_data);
+
+void app_wifi_init(void)
+{
+    bsp_register_callback(app_wifi_handler);
+
+    bsp_wifi_start();
+}
+
+/* Wifi and ip event handler */
+static void app_wifi_handler(void* event_handler_arg,
+                            esp_event_base_t event_base,
+                            int32_t event_id,
+                            void* event_data)
+{
+    if(event_base == WIFI_EVENT)
+    {
+        switch (event_id)
+        {
+            case WIFI_EVENT_STA_START:
+                ESP_LOGI(TAG, "wifi start try to connect");
+                ESP_ERROR_CHECK(esp_wifi_connect());
+                break;
+
+            case WIFI_EVENT_STA_CONNECTED:
+                ESP_LOGI(TAG, "wifi connected");
+                break;
+
+            case WIFI_EVENT_STA_DISCONNECTED:
+                ESP_LOGE(TAG, "wifi disconnected");
+
+                esp_err_t err = esp_wifi_connect();
+                if (err != ESP_OK && err != ESP_ERR_WIFI_NOT_STARTED) {
+                    ESP_LOGE(TAG, "Connection failed: %d", err);
+                }
+                break;
+            default:
+                break;
+        }
+    }
+    else if(event_base == IP_EVENT)
+    {
+        switch (event_id)
+        {
+            case IP_EVENT_STA_GOT_IP:
+                ip_event_got_ip_t* ip_data = (ip_event_got_ip_t*)event_data;
+                ESP_LOGI(TAG, "event got ip: "IPSTR, IP2STR(&ip_data->ip_info.ip));
+                break;
+            
+            case IP_EVENT_STA_LOST_IP:
+                ESP_LOGE(TAG, "event lost ip");
+                break;
+
+            default:
+                break;
+        }
+    }
+}
--- remote_car/src/main.c
+++ remote_car/src/main.c
@@ -2,14 +2,29 @@
 
 #include "bsp_nvs_flash.h"
 #include "bsp_sntp.h"
+
 #include "osal.h"
+
+#include "app_wifi.h"
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
 void app_main()
 {
+    vTaskDelay(pdMS_TO_TICKS(2000)); 
+
     bsp_nvs_flash_start();
 
-    //bsp_start_sntp_time(UA);
+    app_wifi_init();
+
+    vTaskDelay(pdMS_TO_TICKS(10000));
+
+    bsp_start_sntp_time(UA);
+
+    while(1)
+    {
+        ESP_LOGI("MAIN", "Hello! Board is running.");
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
 }
