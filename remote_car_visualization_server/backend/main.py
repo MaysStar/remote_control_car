fc1b33b0dc7da7b64f275bcddcd687a649105498
Danylo20006 <danyk933@gmail.com>
2026-06-22 14:26:58 +0300

    fix: add cloud fallback for ALPHA env variable

--- remote_car_visualization_server/backend/main.py
+++ remote_car_visualization_server/backend/main.py
@@ -3,13 +3,14 @@ import asyncio
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.responses import HTMLResponse
 from pydantic import BaseModel
+import os
+
 
 # Try loading ALPHA from local config, fallback to Environment Variables for cloud deploy
 try:
     from config.settings import ALPHA
 except ModuleNotFoundError:
-    import os
-    ALPHA = float(os.getenv("ALPHA", 0.1))
+    ALPHA = float(os.getenv("ALPHA", 0.8)) 
 
 
 app = FastAPI()
